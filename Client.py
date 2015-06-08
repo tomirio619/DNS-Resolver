d1cce1b72d7057095ed62997f3a1aa38c1931cdb
Justin Mol <jussepusje@gmail.com>
2015-06-08 11:58:23 +0200

    Bertaaaaa

--- Client.py
+++ Client.py
@@ -1,8 +1,7 @@
-import utils
+import random
 
 __author__ = 'tomirio619 & jusser'
 
-from threading import Thread
 import socket
 import struct
 import select
@@ -15,11 +14,20 @@ def packsubstring(substr):
     return out
 
 
-def sendQuery(msg, address='localhost', port=53):
-    query = utils.encode(msg)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = (address, port)
-    sock.sendto(query, server_address)
+def encode(address):
+    #maak DNS query, genereer 16 bit ID (mag niet gebruikt zijn)
+    ID = random.getrandbits(16)
+    FlgsNCodes = 256  # RD = 1
+    out = struct.pack('!6H', ID, FlgsNCodes, 1, 0, 0, 0)
+
+    splittedAddress = address.split(".")
+    buf = bytearray(b'')
+    for str in splittedAddress:
+        length = len(str)
+        out += struct.pack('!B', length)
+        out += packsubstring(str)
+    out += struct.pack('!B2h', 0, 1, 1)
+    return out
 
     whatReady = select.select([sock], [], [])
     if whatReady:
@@ -32,7 +40,11 @@ def sendQuery(msg, address='localhost', port=53):
 
 
 def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print 'Starting client'
+    address = 'localhost'
+    port = 53
+    server_address = (address, port)
 
     # Question bestaat uit query type (QTYPE), query class (QCLASS) en query domain name (QNAME)
     # Zie rfc1035 4.1.2 voor question section format
@@ -40,6 +52,7 @@ def main():
     # http://www.zytrax.com/books/dns/ch15/
     # We houden alleen rekening met Resource Records van type A en CNAME van klasse IN
     # We moeten werken met een byte-array
+<<<<<<< HEAD
     DNSmsgs = [
     "www.mijn-daltons.nl"
     # "mail.fishcom.ru"
@@ -47,6 +60,12 @@ def main():
     for msg in DNSmsgs:
         t = Thread(target = sendQuery, args = (msg, 'localhost', 53))
         t.start()
+=======
+
+    DNSmsg = encode("www.spele.nl")
+    sock.sendto(DNSmsg, server_address)
+
+>>>>>>> parent of a2e3f95... Berta
 
 if __name__ == "__main__":
     main()
--- Resolver.py
+++ Resolver.py
@@ -1,40 +1,13 @@
 __author__ = 'tomirio619'
 
+<<<<<<< HEAD
 from threading import Thread, Lock
 
+=======
+>>>>>>> parent of a2e3f95... Berta
 import socket
 import struct
 import select
-import utils
-import time
-import datetime
-
-cache = dict()
-# TTL of -1 means: never expires
-cache['a.root-servers.net'] = (['198.41.0.4'], -1, '')
-cache['b.root-servers.net'] = (['192.228.79.201'], -1, '')
-cache['c.root-servers.net'] = (['192.33.4.12'], -1, '')
-cache['d.root-servers.net'] = (['199.7.91.13'], -1, '')
-cache['e.root-servers.net'] = (['192.203.230.10'], -1, '')
-cache['f.root-servers.net'] = (['192.5.5.241'], -1, '')
-cache['g.root-servers.net'] = (['192.112.36.4'], -1, '')
-cache['h.root-servers.net'] = (['128.63.2.53'], -1, '')
-cache['i.root-servers.net'] = (['192.36.148.17'], -1, '')
-cache['j.root-servers.net'] = (['192.58.128.30'], -1, '')
-cache['k.root-servers.net'] = (['193.0.14.129'], -1, '')
-cache['l.root-servers.net'] = (['199.7.83.42'], -1, '')
-cache['m.root-servers.net'] = (['202.12.27.33'], -1, '')
-
-debuglevel = 1
-lock = Lock()
-
-
-def log(TAG, tolog, tokens=''):
-    if debuglevel == 1:
-        if tokens != '':
-            print tokens+' '+TAG+':', str(tolog) + ' ' + tokens
-        else:
-            print TAG+':', str(tolog)
 
 
 def makeNonRecursive(query):
@@ -45,6 +18,7 @@ def makeNonRecursive(query):
     return qheader + query[12:]
 
 
+<<<<<<< HEAD
 # Onderstaande functie print de naam van een Resource Record (met extra punt op het einde)
 # def printName(data, result):
 #     nextlen = ord(data[0])
@@ -150,10 +124,21 @@ def checkCache(name):
         else:  # This cachevalue is expired
             cache.pop(name, None)
             return None
+=======
+def skipName(data):
+    nextlen = ord(data[0])          #waarde van data is op een gegeven moment leeg, vandaar de error
+    print 'de waarde van nextlen is ', nextlen
+    print 'de data is als volgt:', data
+    if nextlen > 0:
+        return skipName(data[nextlen + 1:])
+>>>>>>> parent of a2e3f95... Berta
     else:
-        return None
+        data = data[1:]
+        print 'Naam is eraf gesnoepd, we returnen de data:', data
+        return data
 
 
+<<<<<<< HEAD
 # Function that finds IP of a Nameserver Record in the additional records (or in cache)
 def findIP(NS, ARs):
     name = NS[0]
@@ -193,12 +178,45 @@ def resolve(query, dnsIP='192.55.83.30'):   # Default IP is local host
         return cache[name]
 
     ID = struct.unpack('!H', query[0:2])[0]
+=======
+def stripRR(data):
+    data = skipName(data)
+    print 'levert dit al een error op?'
+    #print data
+    data = data[8:]  # Snoep TYPE, CLASS, TTL van data af
+    RDLENGTH = struct.unpack_from('!H', data)[0]
+    #print "Waarde van RDLENGHT is ", RDLENGTH
+    data = data[2+RDLENGTH:]  # Snoep RDLENGTH + RDATA van data af
+    return data
+
+
+def getNextIP(response):
+    header = response[0:12]
+    data = response[12:]
+    print 'de lengte van data voor bewerken is ', len(data)
+    ANCount, NSCount, ARCount = struct.unpack("!6x3H", header)
+    while NSCount > 0:
+        data = stripRR(data)
+        print 'We hebben waarde NSCount van ', NSCount
+        print 'De stripped data heeft een lengte van ', len(data)
+        NSCount -= 1
+
+    print 'Uiteindelijke lengte van data is ', len(data)
+
+    return 'HAII'
+
+
+def resolve(query, dnsIP):
+    # print 'We gaan een query ontleden'
+    # print 'De query is als volgt: ' + query
+
+>>>>>>> parent of a2e3f95... Berta
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(query, (dnsIP, 53))
-
     whatReady = select.select([sock], [], [])
-    if whatReady:  # Pythonic way of saying list is not empty
+    if whatReady != []:
         rcvd = sock.recv(512*8)
+<<<<<<< HEAD
         print utils.isAA(rcvd)
         sock.close()
         oneStep = doOneStep(rcvd)
@@ -226,12 +244,44 @@ def solve(query, dnsIP='192.55.83.30'):
         print answer
         # log('Cachedump', cache, '-')
         writeCacheTo(cache)
+=======
+        # print rcvd
+
+        sock.close()
+        nextIP = getNextIP(rcvd)
+        # resolve(query, nextIP)
+
+
+def printInfo(pkt):
+    length = len(pkt)
+    print '---------------------'
+    print 'IP PACKET\n---------------------'
+    print 'Length: ', length
+    ipHeader = pkt[0:20]
+    (src1, src2, src3, src4, dst1, dst2, dst3, dst4) = struct.unpack('!12x8B', ipHeader)
+    print 'Source address: {}.{}.{}.{}'.format(src1, src2, src3, src4)
+    print 'Dest address: {}.{}.{}.{}'.format(dst1, dst2, dst3, dst4)
+    print '---------------------'
+    print 'UDP PACKET\n---------------------'
+    udpHeader = pkt[20:28]
+    srcprt, destprt, length = struct.unpack('!3H2x', udpHeader)
+    print 'length: ', length
+    print 'Source port: ', srcprt
+    print 'Dest port: ', destprt
+    udpData = pkt[28:28+length]
+    print 'UDP data: ' + udpData
+
+
+def dissect(pkt):
+    ipHeader = pkt[0:20]
+    udpHeader = pkt[20:28]
+    srcprt, destprt, length = struct.unpack('!3H2x', udpHeader)
+    udpData = pkt[28:28+length]
+    return ipHeader, udpHeader, udpData
+>>>>>>> parent of a2e3f95... Berta
 
 
 def main():
-    # TODO: Read cache from file
-    writeCacheTo(cache)
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_RAW)
     print 'Starting resolver'
     address = 'localhost'
@@ -240,10 +290,11 @@ def main():
     sock.bind(server_address)
 
     while True:
-        # We pakken eerst de eerste 160 bits (20 bytes) UDP Header en DNS Header bevat
+        #We pakken eerst de eerste 160 bits (20 bytes) UDP Header en DNS Header bevat
         whatReady = select.select([sock], [], [])
         if whatReady != []:
             pkt = sock.recv(20*80)
+<<<<<<< HEAD
             # utils.printInfo(pkt)
             print utils.isAA(pkt)
             ipHeader, udpHeader, udpData = utils.dissectIP(pkt)
@@ -254,6 +305,16 @@ def main():
                 t.start()
                 # Answer = resolve(udpData, '192.33.4.12')  # root server
                 # Answer = resolve(udpData, 131.174.117.20')
+=======
+            #printInfo(pkt)
+            ipHeader, udpHeader, udpData = dissect(pkt)
+
+            if len(udpData) >= 12:
+                #try:
+                resolve(udpData, '198.41.0.4') #131.174.117.20')
+                #except struct.error:
+                 #   print 'Not a DNS-query'
+>>>>>>> parent of a2e3f95... Berta
 
 
 if __name__ == "__main__":
